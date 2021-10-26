901dabbd56b8f0dc9161b8d32009159eeb00226e
Matthew Ty <matthewty1220@gmail.com>
2021-10-26 06:15:07 -0600

    added workout api

--- src/components/pages/WorkoutEntry/WorkoutEntry.js
+++ src/components/pages/WorkoutEntry/WorkoutEntry.js
@@ -1,11 +1,33 @@
-function WorkoutEntry() {
-      return (
-          <h1>
-              <div>
-                  Workout Entry
-              </div>
-          </h1>
-      );
-  }
-export default WorkoutEntry;
-  
+import React from 'react';
+
+
+class WorkoutEntry extends React.Component {
+
+    state = {
+        loading: true,
+        exercise: null
+    }
+    async componentDidMount() {
+        const url ="https://wger.de/api/v2/exercise/";
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({exercise: data.results[0], loading: false})
+    }
+
+    render() {
+        return (
+        <div>
+            {this.state.loading || !this.state.exercise ? (
+                <div>loading...</div> 
+                ) : (    
+                <div>
+                    <div>{this.state.exercise.name}</div>
+                </div>
+        )}
+        </div>
+        );
+    }    
+}
+    export default WorkoutEntry;
+    
+    
