232944b221a33ba55dbf56d7ce4feb20a32d0d76
Matthew Ty <matthewty1220@gmail.com>
2021-10-27 12:10:31 -0600

    added changes to title

--- src/components/pages/Homepage/Homepage.js
+++ src/components/pages/Homepage/Homepage.js
@@ -12,10 +12,18 @@ import './Homepage.css'
                 <img className="streak-photo" src="./images/streak.png" alt="Streak"/>
             </div>
             <div class="rectangle">
+                <h3 class="text"> </h3>
+            </div>
+            <div class="welcome-to-streak">
                 <h3 class="text"> Welcome to Streak </h3>
+
+            <div class="about-streal">
+                
+            </div>
             </div>
             </div>
         </h1>
+        
     );
-    }   
+}   
 export default Homepage;
