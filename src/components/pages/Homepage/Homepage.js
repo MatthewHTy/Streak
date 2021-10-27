664106ac56c0d06406c03328f82f648f9c344c52
Matthew Ty <matthewty1220@gmail.com>
2021-10-27 12:40:47 -0600

    added changes to homepage

--- src/components/pages/Homepage/Homepage.js
+++ src/components/pages/Homepage/Homepage.js
@@ -17,8 +17,17 @@ import './Homepage.css'
             <div class="welcome-to-streak">
                 <h3 class="text"> Welcome to Streak </h3>
 
-            <div class="about-streak">
-                
+            <div class="rectangle-2">
+                <h3 class="text"> </h3>
+            </div>
+            <div class="what-is-streak">
+                <h3 class="text">How It Works</h3>
+            </div>
+            <div class="rectangle-3">
+                <h3 class="text"> </h3>
+            </div>
+            <div class="what-is-streak-p">
+                <h2 class="text">Streak</h2>
             </div>
             </div>
             </div>
