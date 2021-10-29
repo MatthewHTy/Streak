c2698135b25a02ba9b39c374b78be9f09f2cbddb
Matthew Ty <matthewty1220@gmail.com>
2021-10-29 14:11:15 -0600

    added changes to plan api

--- src/components/pages/Plans/Plans.js
+++ src/components/pages/Plans/Plans.js
@@ -1,39 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useHistory } from 'react-router';
+import { auth, gituser } from '../../../firebase/firebase';
 import './Plans.css';
 
-class Plans extends React.Component {
+function Plans() {
 
-    state = {
-        loading: true,
-        exercise: null
+    const [user, loading] = useAuthState(auth);
+    const history = useHistory();
+  
+    const [userdata, setuserdata] = useState({
+      name: 'user',
+      email: 'email',
+      uid: '0',
+      streak: 0,
+      entries: [],
+      highscore: 0
+    })
+  
+    const gituserdata = async () => {
+      setuserdata(await gituser(user)) 
     }
-    async componentDidMount() {
-        const url ="https://wger.de/api/v2/exercise/";
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data)
-        this.setState({exercise: data.results.exercise[0], loading: false})
-    }
+  
+    useEffect(() => {
+      if (loading) return;
+      if (!user) return history.replace("/");
+      gituserdata();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user, loading]);
 
-    render() {
-        return (
-
-        
-        <div>
-            {this.state.loading || !this.state.exercise ? (
-                <div className="workout-plan-1">
-                    <h1>Retrieving Workout Plans...</h1></div> 
-                ) : (    
-                    <div>
-                    <div className="exercise-1"><h1>{this.state.exercise.name}</h1></div>
-                </div>
-        )}
+    return (
+        <div className="plans-title">
+        <h3 className="text"> </h3>
+        <div className="plans-title-text">
+            <h1>Workout Plans</h1>
         </div>
-        
-            
-
-        );
-    }    
+        </div>
+    )
 }
 
-export default Plans;
+export default Plans
