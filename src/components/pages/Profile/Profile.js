12b04d7216f5f4a1bda4b361b437d692be21e795
Matthew Ty <matthewty1220@gmail.com>
2021-10-29 11:39:29 -0600

    added changes to title

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -1,6 +1,35 @@
 // import Dashboard from "../../../firebase/Dashboard"; 
 
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useHistory } from "react-router";
+import { auth, gituser } from "../../../firebase/firebase";
+
 function Profile() {
+
+    const [user, loading] = useAuthState(auth);
+    const history = useHistory();
+  
+    const [userdata, setuserdata] = useState({
+      name: 'user',
+      email: 'email',
+      uid: '0',
+      streak: 0,
+      entries: [],
+      highscore: 0
+    })
+  
+    const gituserdata = async () => {
+      setuserdata(await gituser(user)) 
+    }
+  
+    useEffect(() => {
+      if (loading) return;
+      if (!user) return history.replace("/");
+      gituserdata();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user, loading]);
+
     return (
     <div>
         <h1>Profile</h1>
--- src/components/pages/WorkoutEntry/WorkoutEntry.js
+++ src/components/pages/WorkoutEntry/WorkoutEntry.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useHistory } from 'react-router';
+import { auth, gituser } from '../../../firebase/firebase';
+import './WorkoutEntry.css'
 
 function WorkoutEntry () {
+
+    const [user, loading] = useAuthState(auth);
+    const history = useHistory();
+  
+    const [userdata, setuserdata] = useState({
+      name: 'user',
+      email: 'email',
+      uid: '0',
+      streak: 0,
+      entries: [],
+      highscore: 0
+    })
+  
+    const gituserdata = async () => {
+      setuserdata(await gituser(user)) 
+    }
+  
+    useEffect(() => {
+      if (loading) return;
+      if (!user) return history.replace("/");
+      gituserdata();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user, loading]);
+
     return (
-        <div>
-            <h1>Workout Entries</h1>
+        <div className="workout-entries-title">
+        <h3 className="text"> </h3>
+        <div className="workout-entries-title-text">
+            <h1>{userdata.name}'s Workout</h1>
+        </div>
         </div>
     )
 }
